23578a26b9e9383690530b93bbc3fd0d987b47d7
muhamistau <islam.taufikurahman@gmail.com>
2019-09-30 21:32:54 +0700

    add conditional and function

---
+++ src/pertemuan1/conditional/Conditional.kt
@@ -0,0 +1,49 @@
+package pertemuan1.conditional
+
+fun main() {
+    // IF
+    val officeOpen = 7
+    val now = 20
+    if (now > officeOpen) {
+        println("office already open")
+    }
+
+    // IF ELSE
+//    val officeOpen = 7
+//    val now = 20
+//    val office: String
+//    if (now > officeOpen) {
+//        office = "Office already open"
+//    } else {
+//        office = "Office is closed"
+//    }
+//
+//    print(office)
+
+    // IF as an Expression
+//    val officeOpen = 7
+//    val now = 20
+//    val office: String
+//    office = if (now > officeOpen) {
+//        "Office already open"
+//    } else {
+//        "Office is closed"
+//    }
+//
+//    print(office)
+
+    // IF ELSE IF
+//    val officeOpen = 7
+//    val now = 7
+//    val office: String
+//    office = if (now > 7) {
+//        "Office already open"
+//    } else if (now == officeOpen){
+//        "Wait a minute, office will be open"
+//    } else {
+//        "Office is closed"
+//    }
+//
+//    print(office)
+
+}
